1f4808e698ed93d066975b390f6f5c344dab5452
Muhammad Bilal <mbss989@gmail.com>
2021-09-19 14:53:45 +0500

    movies repo added

--- app/src/main/java/com/test/app/movies/data/DefualtRepository.kt
+++ app/src/main/java/com/test/app/movies/data/DefualtRepository.kt
@@ -1,5 +1,13 @@
 package com.test.app.movies.data
 
-class DefualtRepository:
-    MovieRepository {
+import com.test.app.movies.data.model.Movie
+
+class DefaultRepository: MovieRepository {
+    override fun getListOfMovies(pageNo: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addMovie(movie: Movie) {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/test/app/movies/data/MovieRepository.kt
+++ app/src/main/java/com/test/app/movies/data/MovieRepository.kt
@@ -1,4 +1,8 @@
 package com.test.app.movies.data
 
+import com.test.app.movies.data.model.Movie
+
 interface MovieRepository {
+    fun getListOfMovies(pageNo:Int)
+    fun addMovie(movie: Movie)
 }
